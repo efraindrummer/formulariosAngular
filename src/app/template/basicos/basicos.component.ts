14e7c6fffe5436079505072b377fa08406c30945
Efrain May <efrain_may_75@hotmail.com>
2021-05-18 09:34:38 -0500

    validaciones

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -20,6 +20,10 @@ export class BasicosComponent implements OnInit {
     return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched;
   }
 
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.touched && this.miFormulario?.controls.precio?.value < 0;
+  }
+
   guardar(){
     console.log(this.miFormulario);
   }
