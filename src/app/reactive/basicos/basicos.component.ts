dacdd0bec202c64fe86322154434be5213d06858
Efrain May <efrain_may_75@hotmail.com>
2021-05-19 13:07:44 -0500

    formBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,10 +9,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class BasicosComponent {
 
-  miFormulario: FormGroup = new FormGroup({
-    'nombre': new FormControl('RTX 3090TI')
-  });
+  /* miFormulario: FormGroup = new FormGroup({
+    nombre      : new FormControl('RTX 3090TI'),
+    precio      : new FormControl(1500),
+    existencias : new FormControl(5),
+  }); */
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['RTX 4080TI'],
+    precio: [0],
+    existencias: [0],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
 }
