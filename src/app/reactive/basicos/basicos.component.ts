0c53d063c1adb88f518b141f4ecbeff59cdd4d0b
Efrain May <efrain_may_75@hotmail.com>
2021-05-19 13:43:21 -0500

    submit del formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   /* miFormulario: FormGroup = new FormGroup({
     nombre      : new FormControl('RTX 3090TI'),
@@ -23,8 +23,26 @@ export class BasicosComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080Ti',
+      precio: 1600
+    })
+  }
+
   campoEsValido( campo: string ){
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
