dc96e6a2636f8881366176c45cf2371c0c876548
Efrain May <efrain_may_75@hotmail.com>
2021-05-28 14:21:18 -0500

    controles al FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -17,6 +17,8 @@ export class DinamicosComponent {
     ], Validators.required )
   });
 
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
   get favoritosArr(){
     return this.miFormulario.get('favoritos') as FormArray;
   }
@@ -27,6 +29,17 @@ export class DinamicosComponent {
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
   }
   
+  agregarFavorito(){
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+    
+    //this.favoritosArr.push( new FormControl(this.nuevoFavorito.value, Validators.required) );
+    this.favoritosArr.push( this.fb.control(this.nuevoFavorito.value, Validators.required) );
+
+    this.nuevoFavorito.reset();
+  }
+
   guardar(){
     if(this.miFormulario.invalid){
       this.miFormulario.markAllAsTouched();
